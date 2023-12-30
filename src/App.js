6d3a23a7c4666d22d5891c97bce85c9a9d8c8075
RileyAlexis <MxRileyAlexis@gmail.com>
2023-12-30 11:56:48 -0600

    Basic material UI layout prepped

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import { useSelector } from "react-redux";
 import Login from "./components/Login/Login";
 
+//Material UI
+import { Grid, Container, Box, Typography } from "@mui/material";
+
+
+
 function App() {
 
 const user = useSelector(store => store.user);
 const userDisplay = JSON.stringify(user);
 
   return (
-    <div className="App">
-      Aura App skeleton <br />
+    <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} maxWidth="xl">
+
+      <Grid item xs={12} sm={4} md={4} lg={4} 
+            sx={{ border: 1 }}>
+      <Typography variant="h3">
+      Aura
+      </Typography>
+      </Grid>
+     
+     <Grid item xs={12} sm={4} md={4} lg={4} sx={{ border: 1 }}>
       {userDisplay}
-      
+      </Grid>
+
+
+
+
+      {user.id &&
+      <Grid item xs={12} md={12} lg={8}>
       <Login />
-      
-    </div>
+      </Grid>
+    
+      }
+    </Grid>
   );
 }
 
